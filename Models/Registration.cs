171b03b1b3bf0de39eb35004ad66f0aa4511ccb2
Ujwala2Uk <roopaumesha2@gmail.com>
2022-10-12 20:46:52 +0530

    Add project files.

---
+++ Models/Registration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ANGULAR_API.Models
+{
+    public partial class Registration
+    {
+        [Key]
+        public int Id { get; set; }
+      
+        public string? Username { get; set; }
+
+        public string? Useremail { get; set; }
+
+        public string? Password { get; set; }
+        public string? Mobile { get; set; }
+    }
+}
